d4e3884f4d0558b33d98a5170ea4d4e72b6b54d6
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-16 17:25:49 -0500

    seccion 5 video 33 comstantes

--- seccion4/ejercicios.go
+++ seccion4/ejercicios.go
@@ -4,12 +4,11 @@ import (
 	"fmt"
 )
 
+const (
+	a     = 42
+	b int = 43
+)
+
 func main() {
-	a := (42 == 42)
-	b := (42 <= 43)
-	c := (42 >= 43)
-	d := (42 != 43)
-	e := (42 < 43)
-	f := (42 > 43)
-	fmt.Println(a, b, c, d, e, f)
+	fmt.Println(a, b)
 }
