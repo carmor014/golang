029f66c07af72207458b492da4abcb931f2760c2
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-16 12:17:41 -0500

    seccion 3 ejercicio 5

--- seccion3/ejercicios.go
+++ seccion3/ejercicios.go
@@ -5,10 +5,16 @@ import "fmt"
 type dinero int
 
 var x dinero
+var y int
 
 func main() {
 	fmt.Printf("%v\n", x)
 	fmt.Printf("%T\n", x)
+
 	x = 42
 	fmt.Println(x)
+
+	y = int(x + 58)
+	fmt.Println(y)
+	fmt.Printf("%T", y)
 }
