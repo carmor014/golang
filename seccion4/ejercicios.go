c173fde3125e9d1d275fa1a815383c44df14eec5
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-16 17:28:51 -0500

    seccion 5 video 35 imprimir un string no interpretado

--- seccion4/ejercicios.go
+++ seccion4/ejercicios.go
@@ -5,8 +5,12 @@ import (
 )
 
 func main() {
-	a := 42
-	fmt.Printf("%d\t%b\t%#x\n", a, a, a)
-	b := a << 1
-	fmt.Printf("%d\t%b\t%#x", b, b, b)
+	a := `here is something
+	as 
+	a 
+	raw string
+	literal
+	"you see"
+	another thing`
+	fmt.Println(a)
 }
