d0ee5e5b3217ba21aa1265f1901f3b00a83a8e20
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-16 17:16:28 -0500

    seccion 4 video 29 constante iota

--- seccion3/main.go
+++ seccion3/main.go
@@ -4,40 +4,23 @@ import (
 	"fmt"
 )
 
-const a = 42
+const (
+	a = iota
+	b
+	c
+)
+
+const (
+	d = iota
+	e
+	f
+)
 
 func main() {
-	if a != 0 {
-		println("Hola constantes")
-	}
-	s1 := "Hola mundo"
-	s2 := `Esta es una línea 
-	partida.`
-
-	fmt.Println(s1)
-	fmt.Printf("El tipo de s1 es: %T\n", s1)
-
-	fmt.Println(s2)
-	fmt.Printf("El tipo de s2 es: %T\n", s2)
-
-	fmt.Println("")
-
-	bs := []byte(s1)
-	fmt.Println(bs)
-	fmt.Printf("%T", bs)
-
-	fmt.Println("")
-
-	for i := 0; i < len(s1); i++ {
-		fmt.Printf("%#U ", s1[i])
-	}
-
-	fmt.Println("")
-
-	for i, v := range s1 {
-		fmt.Printf("El el índice %d el valor es %#x\n", i, v)
-	}
-
-	fmt.Printf("Con el verbo %q indico que se imprima el string %s", "%s", s1)
-
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
 }
