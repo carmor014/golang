694cfa05b908cbcc770c86e05c8df4bc0bb79f20
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 17:14:37 -0500

    seccion 11 video 87 ejercicio#1 struct

--- seccion10/main.go
+++ seccion10/main.go
@@ -1,19 +1,26 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type persona struct {
+	nombre   string
+	apellido string
+	saborFav []string
+}
 
 func main() {
-	p1 := struct {
-		first string
-		last  string
-		age   int
-	}{
-		first: "Eduar",
-		last:  "Tua",
-		age:   31,
+	p1 := persona{
+		nombre:   "Carlos",
+		apellido: "Mora",
+		saborFav: []string{
+			"chocolate",
+			"arequipe",
+			"chicle",
+		},
 	}
-
-	fmt.Println(p1)
+	fmt.Println(p1.nombre)
+	fmt.Println(p1.apellido)
+	for i, v := range p1.saborFav {
+		fmt.Println("\t", i, v)
+	}
 }
