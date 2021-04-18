230cd4e465069fb2048dd7d7d129a9c2277fd644
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 11:32:03 -0500

    seccion 6 video 45 conditional if, else if, else

--- seccion6/main.go
+++ seccion6/main.go
@@ -3,13 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	if x := 42; x == 42 {
-		fmt.Println("001")
+	x := 434
+	if x == 40 {
+		fmt.Println("El valor es 40")
+	} else if x == 41 {
+		fmt.Println("El valor es 41")
+	} else if x == 42 {
+		fmt.Println("El valor es 42")
+	} else if x == 43 {
+		fmt.Println("El valor es 43")
+	} else {
+		fmt.Println("El valor no es 40, 41, 42, ni 43")
 	}
-
-	y := 30
-	if y == 30 {
-		fmt.Println(y)
-	}
-	fmt.Println(y)
 }
