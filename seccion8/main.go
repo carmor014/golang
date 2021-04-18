f4713ba5489db0ba45532e9bd8d701d669dfe67f
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 13:52:09 -0500

    seccion 8 video 68 el arreglo subyacente de slices

--- seccion8/main.go
+++ seccion8/main.go
@@ -5,11 +5,16 @@ import (
 )
 
 func main() {
-	et := []string{"Eduar", "Tua", "Crossfit", "Baseball", "Montañismo"}
-	fmt.Println(et)
-	jl := []string{"Jacinto", "Lopez", "Correr", "Nadar", "Bailar"}
-	fmt.Println(jl)
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	fmt.Println(x)
 
-	vp := [][]string{et, jl}
-	fmt.Println(vp)
+	y := append(x[:2], x[5:]...) // the same underlying array stores the value of the new slice
+
+	fmt.Println(x)
+	fmt.Println("len", len(x))
+	fmt.Println("cap", cap(x))
+
+	fmt.Println(y)
+	fmt.Println("len", len(y))
+	fmt.Println("cap", cap(y))
 }
