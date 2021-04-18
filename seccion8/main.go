fdb60e3a135b820543f06d4c43ea32d522a82763
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 13:08:18 -0500

    seccion 8 video 64 slice - Añadiendo a un slice

--- seccion8/main.go
+++ seccion8/main.go
@@ -5,20 +5,13 @@ import (
 )
 
 func main() {
-	x := []int{1, 2, 3, 4, 5}
+	x := []int{4, 5, 7, 8, 42}
 	fmt.Println(x)
-	fmt.Println(len(x))
-	fmt.Println(cap(x))
-	fmt.Println(x[:])
-	fmt.Println(x[1:3])
-	fmt.Println(x[2:4])
+	x = append(x, 44, 55, 66)
+	fmt.Println(x)
 
-	for i, v := range x {
-		fmt.Println(i, " ", v)
-	}
-
-	for i := 0; i < len(x); i++ {
-		fmt.Println(i, x[i])
-	}
+	y := []int{333, 444, 666}
+	x = append(x, y...)
+	fmt.Println(x)
 
 }
