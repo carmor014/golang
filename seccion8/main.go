dd439d9142f0d1295aa88791fd243625cacc5692
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 13:05:20 -0500

    seccion 8 video 63 Dividiendo un slice

--- seccion8/main.go
+++ seccion8/main.go
@@ -7,14 +7,18 @@ import (
 func main() {
 	x := []int{1, 2, 3, 4, 5}
 	fmt.Println(x)
-	fmt.Println(x[0]) // cada uno tiene una posicion empezando desde 0
-	fmt.Println(x[1])
-	fmt.Println(x[2])
-	fmt.Println(x[3])
-	fmt.Println(x[4])
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+	fmt.Println(x[:])
+	fmt.Println(x[1:3])
+	fmt.Println(x[2:4])
 
-	for i, v := range x { //dos variables para este caso indice  valor
+	for i, v := range x {
 		fmt.Println(i, " ", v)
+	}
+
+	for i := 0; i < len(x); i++ {
+		fmt.Println(i, x[i])
 	}
 
 }
