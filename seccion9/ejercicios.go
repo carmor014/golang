3b8c963ebf38ddb5b5b5e505055dbdb786b268f3
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 15:58:44 -0500

    seccion 9 video 78 ejercicio#7 slice multidimensional

--- seccion9/ejercicios.go
+++ seccion9/ejercicios.go
@@ -5,13 +5,18 @@ import (
 )
 
 func main() {
-	x := make([]string, 50, 50)
-	x = []string{` Alabama`, ` Alaska`, ` Arizona`, ` Arkansas`, ` California`, ` Colorado`, ` Connecticut`, ` Delaware`, ` Florida`, ` Georgia`, ` Hawaii`, ` Idaho`, ` Illinois`, ` Indiana`, ` Iowa`, ` Kansas`, ` Kentucky`, ` Louisiana`, ` Maine`, ` Maryland`, ` Massachusetts`, ` Michigan`, ` Minnesota`, ` Mississippi`, ` Missouri`, ` Montana`, ` Nebraska`, ` Nevada`, ` New Hampshire`, ` New Jersey`, ` New Mexico`, ` New York`, ` North Carolina`, ` North Dakota`, ` Ohio`, ` Oklahoma`, ` Oregon`, ` Pennsylvania`, ` Rhode Island`, ` South Carolina`, ` South Dakota`, ` Tennessee`, ` Texas`, ` Utah`, ` Vermont`, ` Virginia`, ` Washington`, ` West Virginia`, ` Wisconsin`, ` Wyoming`}
-	fmt.Println(x)
-	fmt.Println(len(x))
-	fmt.Println(cap(x))
+	xs1 := []string{"Batman", "Jefe", "Vestido de negro"}
+	xs2 := []string{"Robin", "Ayudante", "Vestido de colores"}
+	fmt.Println(xs1)
+	fmt.Println(xs2)
 
-	for i := 0; i < len(x); i++ {
-		fmt.Println(i, x[i])
+	xxs := [][]string{xs1, xs2}
+	fmt.Println(xxs)
+
+	for i, reg := range xxs {
+		fmt.Println("Registro: ", i)
+		for j, val := range reg {
+			fmt.Printf("\tíndice de posición: %v\tvalor: \t %v \n", j, val)
+		}
 	}
 }
