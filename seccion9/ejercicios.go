c17770fe546fb23f1926379a37aaae0cf4366398
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 15:49:22 -0500

    seccion 9 video 76 ejercicio#5 manejo slice eliminando

--- seccion9/ejercicios.go
+++ seccion9/ejercicios.go
@@ -6,12 +6,8 @@ import (
 
 func main() {
 	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
-	x = append(x, 52)
-	fmt.Println(x)
-	x = append(x, 53, 54, 55)
 	fmt.Println(x)
 
-	y := []int{56, 57, 58, 59, 60}
-	x = append(x, y...)
+	x = append(x[:3], x[6:]...) // [42, 43, 44, 48, 49, 50, 51]
 	fmt.Println(x)
 }
