9339d8ae0a46719f059dfbc255819db242c755b4
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 13:15:44 -0500

    seccion 8 video 66 slice - make capacidad maxima arreglo subyacente

--- seccion8/main.go
+++ seccion8/main.go
@@ -5,16 +5,32 @@ import (
 )
 
 func main() {
-	x := []int{4, 5, 7, 8, 42}
+	x := make([]int, 10, 12)
 	fmt.Println(x)
-	x = append(x, 44, 55, 66)
-	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+	x[0] = 42
+	x[9] = 999
 
-	y := []int{333, 444, 666}
-	x = append(x, y...)
 	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
 
-	x = append(x[:2], x[4:]...)
+	x = append(x, 3423)
+
 	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
 
+	x = append(x, 3424)
+
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
+
+	x = append(x, 3425)
+
+	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
 }
