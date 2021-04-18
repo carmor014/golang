ba1f2e9798e9c8668753c2ccdb9a152566be8990
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 11:08:40 -0500

    seccion 6 video 42 ciclo for break y continue

--- seccion6/main.go
+++ seccion6/main.go
@@ -4,26 +4,16 @@ import "fmt"
 
 func main() {
 
-	//for init; condition; post {}
-	for i := 0; i <= 10; i++ {
-		fmt.Printf("El ciclo externo %d\n", i)
-		for j := 0; j < 3; j++ {
-			fmt.Printf("\t\tEl ciclo interno %d\n", j)
-		}
-	}
+	x := 1
 
-	//for con una sola condicion es infinito
-	k := 11
-	for k < 10 {
-		fmt.Println(k)
-	}
-
-	//como salir de un fr infinito
-	m := 5
 	for {
-		if m > 4 {
+		x++
+		if x > 10 {
 			break
 		}
-		fmt.Println(m)
+		if x%2 != 0 {
+			continue
+		}
+		fmt.Println(x)
 	}
 }
