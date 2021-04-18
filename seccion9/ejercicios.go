a577feb2f19a20e5fc6a4d5a4fba6f5eec22d614
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 15:44:50 -0500

    seccion 9 video 74 crea un slice e imprime

--- seccion9/ejercicios.go
+++ seccion9/ejercicios.go
@@ -5,9 +5,10 @@ import (
 )
 
 func main() {
-	x := []int{42, 43, 44, 45, 46}
-	for i, v := range x {
-		fmt.Println(i, v)
-	}
-	fmt.Printf("%T\n", x)
+	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+	fmt.Println(x[:5])
+	fmt.Println(x[5:])
+	fmt.Println(x[2:7])
+	fmt.Println(x[1:6])
+	fmt.Println(x)
 }
