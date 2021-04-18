bd5870d626463670ebfe2a2ee0a732fa7e35ddc5
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 16:08:59 -0500

    seccion 9 video 81 ejercicio#10 map eliminando registro

--- seccion9/ejercicios.go
+++ seccion9/ejercicios.go
@@ -5,20 +5,22 @@ import (
 )
 
 func main() {
-	x := map[string][]string{
-		`eduar_tua`:    []string{`computadoras`, `montañas`, `playa`},
-		`carlos_ramon`: []string{`leer`, `comprar`, `música`},
-		`juan_bimba`:   []string{`helado`, `pintar`, `bailar`},
+	m := map[string][]string{
+		`bond_james`:      []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: []string{`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           []string{`Being evil`, `Ice cream`, `Sunsets`},
 	}
 
 	// fmt.Println(m)
 
-	x[`luis_perez`] = []string{`trabajar`, `playa`, `cerveza`}
+	m[`fleming_ian`] = []string{`steaks`, `cigars`, `espionage`}
 
-	for llave, valor := range x {
-		fmt.Println("Registro:", llave)
-		for i, val := range valor {
-			fmt.Println("\t", i, val)
+	delete(m, `no_dr`)
+
+	for k, v := range m {
+		fmt.Println("This is the record for", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
 		}
 	}
 }
