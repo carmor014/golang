075ce706a77ecc130ce0df4abae0e1c7dc4fb6fc
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 18:14:43 -0500

    seccion 12 video 94 defer como posponer una funcion

--- seccion12/main.go
+++ seccion12/main.go
@@ -1,21 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	xi := []int{2, 3, 4, 5, 6, 7, 8, 9}
-	x := sum(xi...)
-	fmt.Println("El valor total almacenado en la varible es:", x)
+	defer foo()
+	bar()
 }
 
-func sum(x ...int) int {
-	fmt.Println(x)
-	fmt.Printf("%T\n", x)
+func foo() {
+	fmt.Println("foo")
+}
 
-	suma := 0
-	for i, v := range x {
-		suma += v
-		fmt.Println("El valor en el indice", i, "suma", v, "altotal, quedando", suma)
-	}
-	return suma
+func bar() {
+	fmt.Println("bar")
 }
