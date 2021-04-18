219b5b8f3efc68efd140e2fec307bc94d6e7534f
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 11:37:22 -0500

    seccion 6 video 46 imprimiendo numeros pares

--- seccion6/main.go
+++ seccion6/main.go
@@ -3,16 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	x := 434
-	if x == 40 {
-		fmt.Println("El valor es 40")
-	} else if x == 41 {
-		fmt.Println("El valor es 41")
-	} else if x == 42 {
-		fmt.Println("El valor es 42")
-	} else if x == 43 {
-		fmt.Println("El valor es 43")
-	} else {
-		fmt.Println("El valor no es 40, 41, 42, ni 43")
+	for i := 0; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
 	}
 }
