18292239524f9bb3732630c3ffb50c6baf8351c4
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 19:43:31 -0500

    seccion 12 video 98 funciones anonimas

--- seccion12/main.go
+++ seccion12/main.go
@@ -2,66 +2,18 @@ package main
 
 import "fmt"
 
-type persona struct {
-	nombre   string
-	apellido string
-}
-
-type agenteSecreto struct {
-	persona
-	lpm bool
-}
-
-func (a agenteSecreto) presentar() {
-	fmt.Println("soy", a.nombre, a.apellido)
-}
-
-func (p persona) presentar() {
-	fmt.Println("soy", p.nombre, p.apellido)
-}
-
-//decarar una interface
-type humano interface {
-	//solo agrego metodos
-	presentar()
-}
-
-//puede recibir persona o un agenteSecreto
-func bar(h humano) {
-	switch h.(type) {
-	case persona:
-		fmt.Println("switch de persona: ", h.(persona).nombre)
-	case agenteSecreto:
-		fmt.Println("switch de persona: ", h.(agenteSecreto).nombre)
-	}
-	fmt.Println("fui pasado a la funcion bar", h)
-}
-
 func main() {
-	as1 := agenteSecreto{
-		persona: persona{
-			nombre:   "Carlos",
-			apellido: "Mora",
-		},
-		lpm: true,
-	}
-	as2 := agenteSecreto{
-		persona: persona{
-			nombre:   "Maria",
-			apellido: "Guz",
-		},
-		lpm: true,
-	}
-	p := persona{
-		nombre:   "Carito",
-		apellido: "Gomez",
-	}
+	foo()
 
-	fmt.Println(as1)
-	as1.presentar()
-	as2.presentar()
+	func() {
+		fmt.Println("La funcion es anonima")
+	}()
 
-	bar(as1)
-	bar(as2)
-	bar(p)
+	func(x int) {
+		fmt.Println("La funcion es anonima con unparametro", x)
+	}(42)
+}
+
+func foo() {
+	fmt.Println("foo se ejecuto")
 }
