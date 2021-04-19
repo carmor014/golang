0bc51d654f3b78ccbf34571018041f093309e91e
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 21:41:18 -0500

    seccion 12 video 103 Recursividad

--- seccion12/main.go
+++ seccion12/main.go
@@ -3,20 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	a := incrementor()
-	b := incrementor()
-	fmt.Println(a())
-	fmt.Println(a())
-	fmt.Println(a())
-	fmt.Println(b())
-	fmt.Println(b())
-	fmt.Println(b())
+	n1 := factorial(4)
+	fmt.Println(n1)
+
+	n2 := cicloFac(4)
+	fmt.Println(n2)
 }
 
-func incrementor() func() int {
-	var x int
-	return func() int {
-		x++
-		return x
+func factorial(n int) int {
+	//caso base cuando n = 0
+	if n == 0 {
+		return 1
 	}
+	return n * factorial(n-1)
+}
+
+//Haciendolo con ciclos
+func cicloFac(n int) int {
+	total := 1
+	for ; n > 0; n-- {
+		total *= n
+	}
+	return total
 }
