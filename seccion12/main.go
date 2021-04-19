dafba941e1cb1eb2d2cbe8bab5f886d1a5eb278e
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 21:05:48 -0500

    seccion 12 video 101 Callbacks

--- seccion12/main.go
+++ seccion12/main.go
@@ -3,13 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	x := bar()
-	i := x()
-	fmt.Println(i)
+	ii := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	s := suma(ii...)
+	fmt.Println(s)
+
+	s1 := sumaPares(suma, ii...)
+	fmt.Println("El total e pares es: ", s1)
+
+	s2 := sumaImpares(suma, ii...)
+	fmt.Println("El total e impares es: ", s2)
 }
 
-func bar() func() int {
-	return func() int {
-		return 1492
+func suma(xi ...int) int {
+	total := 0
+	for _, v := range xi {
+		total += v
 	}
+	return total
+}
+
+func sumaPares(f func(xi ...int) int, vi ...int) int {
+	var y []int
+	for _, v := range vi {
+		if v%2 == 0 {
+			y = append(y, v)
+		}
+	}
+	return f(y...)
+}
+
+func sumaImpares(f func(xi ...int) int, vi ...int) int {
+	var y []int
+	for _, v := range vi {
+		if v%2 != 0 {
+			y = append(y, v)
+		}
+	}
+	return f(y...)
 }
