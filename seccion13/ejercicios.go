76bd269ddbc0f23d91cc550bfc0f207378145786
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 22:49:12 -0500

    seccion 13 video 106 ejercicio#3 funcion con defer

--- seccion13/ejercicios.go
+++ seccion13/ejercicios.go
@@ -5,27 +5,13 @@ import (
 )
 
 func main() {
-	ii := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	n := foo(ii...)
-	fmt.Println(n)
-
-	ii2 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	n2 := bar(ii2)
-	fmt.Println(n2)
+	defer foo()
+	fmt.Println("Hello, playground")
 }
 
-func foo(xi ...int) int {
-	total := 0
-	for _, v := range xi {
-		total += v
-	}
-	return total
-}
-
-func bar(xi []int) int {
-	total := 0
-	for _, v := range xi {
-		total += v
-	}
-	return total
+func foo() {
+	defer func() {
+		fmt.Println("foo DEFER ran")
+	}()
+	fmt.Println("foo ran")
 }
