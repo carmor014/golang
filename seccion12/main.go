40393425f10bcaf9652a15257ba8b75124d655a0
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-18 21:23:18 -0500

    seccion 12 video 102 Closure ejemplo # 2

--- seccion12/main.go
+++ seccion12/main.go
@@ -1,24 +1,22 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var x int
-	fmt.Println(x)
-	x++
-
-	{
-		y := 42
-		fmt.Println(y)
-	}
-	// fmt.Println(y)
-
-	fmt.Println(x)
-	foo()
+	a := incrementor()
+	b := incrementor()
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(b())
+	fmt.Println(b())
+	fmt.Println(b())
 }
 
-func foo() {
-	fmt.Println("hello")
+func incrementor() func() int {
+	var x int
+	return func() int {
+		x++
+		return x
+	}
 }
