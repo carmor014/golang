c2a1900af4b6934f431055dd16666fbc38d4441b
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-20 17:42:15 -0500

    seccion 16 video 123 Interfaz Writer

--- seccion16/main.go
+++ seccion16/main.go
@@ -1,36 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io"
+	"os"
 )
 
-type persona struct {
-	Nombre   string `json:"Nombre"`
-	Apellido string `json:"Apellido"`
-	Edad     int    `json:"Edad"`
-}
-
 func main() {
-	s := `[{"Nombre":"James","Apellido":"Bond","Edad":32},{"Nombre":"Peggy","Apellido":"Bond","Edad":32}]`
-	bs := []byte(s)
-
-	fmt.Printf("%T\n", s)
-	fmt.Printf("%T\n", bs)
-
-	//persona := []persona{}
-	var personas []persona
-
-	err := json.Unmarshal(bs, &personas)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println("Toda la data", personas)
-
-	for i, v := range personas {
-		fmt.Println("\nPERSONA NUMERO", i+1)
-		fmt.Println(v.Nombre, v.Apellido, v.Edad)
-	}
-
+	fmt.Println("Hello")
+	fmt.Fprintln(os.Stdout, "Hello")
+	io.WriteString(os.Stdout, "Hello")
 }
