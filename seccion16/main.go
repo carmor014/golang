46cfbcf86175bd0192046894558d4de650fb8897
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-20 18:06:36 -0500

    seccion 16 video 125 sort personalizado por nombre

--- seccion16/main.go
+++ seccion16/main.go
@@ -10,21 +10,22 @@ type Persona struct {
 	Edad   int
 }
 
-type PorEdad []Persona
+type PorNombre []Persona
 
-func (a PorEdad) Len() int           { return len(a) }
-func (a PorEdad) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a PorEdad) Less(i, j int) bool { return a[i].Edad < a[j].Edad }
+func (a PorNombre) Len() int           { return len(a) }
+func (a PorNombre) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a PorNombre) Less(i, j int) bool { return a[i].Nombre < a[j].Nombre }
 
 func main() {
-	p1 := Persona{"James", 32}
-	p2 := Persona{"Moneypenny", 27}
-	p3 := Persona{"Q", 64}
-	p4 := Persona{"M", 56}
+	p1 := Persona{"Eduar", 32}
+	p2 := Persona{"María", 25}
+	p3 := Persona{"Carolina", 56}
+	p4 := Persona{"Adriana", 60}
 
 	personas := []Persona{p1, p2, p3, p4}
 
 	fmt.Println(personas)
-	sort.Sort(PorEdad(personas))
+
+	sort.Sort(PorNombre(personas))
 	fmt.Println(personas)
 }
