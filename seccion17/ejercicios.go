da9812328d6f832a7f8376e188e168b0bc7536ab
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-21 08:26:58 -0500

    seccion 17 video 130 ejercicio#4 ordenamiento slice int y string

--- seccion17/ejercicios.go
+++ seccion17/ejercicios.go
@@ -1,59 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
+	"sort"
 )
 
-type usuario struct {
-	Nombre   string
-	Apellido string
-	Edad     int
-	Dichos   []string
-}
-
 func main() {
-	u1 := usuario{
-		Nombre:   "Eduar",
-		Apellido: "Tua",
-		Edad:     32,
-		Dichos: []string{
-			"Cachicamo diciéndole a morrocoy conchudo",
-			"La mona, aunque se vista de seda, mona se queda",
-			"Poco a poco se anda lejos",
-		},
-	}
+	xi := []int{5, 8, 2, 43, 17, 987, 14, 12, 21, 1, 4, 2, 3, 93, 13}
+	xs := []string{"Una", "persona", "que", "nunca", "ha", "cometido", "un", "error", "nunca", "intenta", "nada", "nuevo"}
 
-	u2 := usuario{
-		Nombre:   "Carlos",
-		Apellido: "Pérez",
-		Edad:     27,
-		Dichos: []string{
-			"Camarón que se duerme se lo lleva la corriente",
-			"A ponerse las alpargatas que lo que viene es joropo",
-			"No gastes pólvora en zamuro",
-		},
-	}
+	fmt.Println(xi)
+	sort.Ints(xi)
+	fmt.Println(xi)
 
-	u3 := usuario{
-		Nombre:   "Che",
-		Apellido: "López",
-		Edad:     54,
-		Dichos: []string{
-			"Ni lava ni presta la batea",
-			"Hijo de gato, caza ratón",
-			"Más vale pájaro en mano que cien volando",
-		},
-	}
-
-	usuarios := []usuario{u1, u2, u3}
-
-	fmt.Println(usuarios)
-
-	err := json.NewEncoder(os.Stdout).Encode(usuarios)
-	if err != nil {
-		fmt.Println("Hicimos algo mal, este es el error:", err)
-	}
-
+	fmt.Println(xs)
+	sort.Strings(xs)
+	fmt.Println(xs)
 }
