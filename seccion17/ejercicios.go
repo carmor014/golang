b20078c434f6d816e80155c27cbf839e52b42338
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-21 08:16:24 -0500

    seccion 17 video 128 ejercicio#2 convertir de json a tipo dato de go

--- seccion17/ejercicios.go
+++ seccion17/ejercicios.go
@@ -5,34 +5,31 @@ import (
 	"fmt"
 )
 
-type usuario struct {
-	Nombre string
-	Edad   int
+type persona struct {
+	Nombre   string
+	Apellido string
+	Edad     int
+	Dichos   []string
 }
 
 func main() {
-	u1 := usuario{
-		Nombre: "Eduar",
-		Edad:   32,
-	}
+	s := `[{"Nombre":"Eduar","Apellido":"Tua","Edad":32,"Dichos":["Cachicamo diciéndole a morrocoy conchudo","La mona, aunque se vista de seda, mona se queda","Poco a poco se anda lejos"]},{"Nombre":"Carlos","Apellido":"Pérez","Edad":27,"Dichos":["Camarón que se duerme se lo lleva la corriente","A ponerse las alpargatas que lo que viene es joropo","No gastes pólvora en zamuro"]},{"Nombre":"M","Apellido":"Hmmmm","Edad":54,"Dichos":["Ni lava ni presta la batea","Hijo de gato, caza ratón","Más vale pájaro en mano que cien volando"]}]`
+	fmt.Println(s)
 
-	u2 := usuario{
-		Nombre: "Juan",
-		Edad:   27,
-	}
+	var personas []persona
 
-	u3 := usuario{
-		Nombre: "Che",
-		Edad:   54,
-	}
-
-	usuarios := []usuario{u1, u2, u3}
-
-	fmt.Println(usuarios)
-
-	bs, err := json.Marshal(usuarios)
+	err := json.Unmarshal([]byte(s), &personas)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(bs))
+
+	fmt.Println(personas)
+
+	for i, persona := range personas {
+		fmt.Println("Persona #", i+1)
+		fmt.Println("\t", persona.Nombre, persona.Apellido, persona.Edad)
+		for _, dichos := range persona.Dichos {
+			fmt.Println("\t\t", dichos)
+		}
+	}
 }
