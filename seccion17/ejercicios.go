ba8fbe6ebf2cd3eb1d58afda071aff85ad67fbad
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-21 08:27:40 -0500

    seccion 17 video 131 ejercicio#5 ordenamiento personalizado por nombre y apellido

--- seccion17/ejercicios.go
+++ seccion17/ejercicios.go
@@ -5,15 +5,86 @@ import (
 	"sort"
 )
 
+type usuario struct {
+	Nombre   string
+	Apellido string
+	Edad     int
+	Dichos   []string
+}
+
+type PorEdad []usuario
+
+func (a PorEdad) Len() int           { return len(a) }
+func (a PorEdad) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a PorEdad) Less(i, j int) bool { return a[i].Edad < a[j].Edad }
+
+type PorApellido []usuario
+
+func (l PorApellido) Len() int           { return len(l) }
+func (l PorApellido) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l PorApellido) Less(i, j int) bool { return l[i].Apellido < l[j].Apellido }
+
 func main() {
-	xi := []int{5, 8, 2, 43, 17, 987, 14, 12, 21, 1, 4, 2, 3, 93, 13}
-	xs := []string{"Una", "persona", "que", "nunca", "ha", "cometido", "un", "error", "nunca", "intenta", "nada", "nuevo"}
+	u1 := usuario{
+		Nombre:   "Eduar",
+		Apellido: "Tua",
+		Edad:     32,
+		Dichos: []string{
+			"Cachicamo diciéndole a morrocoy conchudo",
+			"La mona, aunque se vista de seda, mona se queda",
+			"Poco a poco se anda lejos",
+		},
+	}
 
-	fmt.Println(xi)
-	sort.Ints(xi)
-	fmt.Println(xi)
+	u2 := usuario{
+		Nombre:   "Carlos",
+		Apellido: "Pérez",
+		Edad:     27,
+		Dichos: []string{
+			"Camarón que se duerme se lo lleva la corriente",
+			"A ponerse las alpargatas que lo que viene es joropo",
+			"No gastes pólvora en zamuro",
+		},
+	}
 
-	fmt.Println(xs)
-	sort.Strings(xs)
-	fmt.Println(xs)
+	u3 := usuario{
+		Nombre:   "Che",
+		Apellido: "López",
+		Edad:     54,
+		Dichos: []string{
+			"Ni lava ni presta la batea",
+			"Hijo de gato, caza ratón",
+			"Más vale pájaro en mano que cien volando",
+		},
+	}
+
+	usuarios := []usuario{u1, u2, u3}
+	for _, v := range usuarios {
+		fmt.Println(v.Nombre, v.Apellido, v.Edad)
+		sort.Strings(v.Dichos)
+		for _, v := range v.Dichos {
+			fmt.Println("\t", v)
+		}
+	}
+
+	fmt.Println("-------")
+
+	sort.Sort(PorEdad(usuarios))
+	for _, v := range usuarios {
+		fmt.Println(v.Nombre, v.Apellido, v.Edad)
+		for _, v := range v.Dichos {
+			fmt.Println("\t", v)
+		}
+	}
+
+	fmt.Println("-------")
+
+	sort.Sort(PorApellido(usuarios))
+	for _, v := range usuarios {
+		fmt.Println(v.Nombre, v.Apellido, v.Edad)
+		for _, v := range v.Dichos {
+			fmt.Println("\t", v)
+		}
+	}
+
 }
