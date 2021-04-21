07eebebce9febded58b54bbb59f403e8e20e4668
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-21 14:07:41 -0500

    seccion 21 video 160 error porque es un canal de recibe

--- seccion21/main.go
+++ seccion21/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	ca := make(chan<- int, 2) //pueda dejar en el canal un valor
+	ca := make(<-chan int, 2) //pueda dejar en el canal un valor
 
+	//recibir  del canal por esto falla
 	ca <- 42
 	ca <- 43
 
-	//Escritura del canal por esto falla
 	fmt.Println(<-ca)
 	fmt.Println(<-ca)
 	fmt.Println("------------")
