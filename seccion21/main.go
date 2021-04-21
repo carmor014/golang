79deb87102f9ca51b797f2812e6cd2435540185f
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-21 11:35:46 -0500

    seccion 21 video 159 gorutina pase bufer exitoso parte 2

--- seccion21/main.go
+++ seccion21/main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	ca := make(chan int, 1) //pueda dejar en el canal un valor
+	ca := make(chan int, 2) //pueda dejar en el canal un valor
 
 	ca <- 42
 	ca <- 43
 
+	fmt.Println(<-ca)
 	fmt.Println(<-ca)
 }
