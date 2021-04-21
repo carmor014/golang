47e9205315cfce33965f822ea251511b39877ee1
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-21 09:13:59 -0500

    seccion 18 video 136 ejercicio gourutinasy uso Mutex para hacerlo correr con go run -race seccion.....

--- seccion18/main.go
+++ seccion18/main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 )
 
-func doSomething(x int) int {
-	return x * 5
-}
-
 func main() {
-	ch := make(chan int)
-	go func() {
-		ch <- doSomething(5)
-	}()
-	fmt.Println(<-ch)
+	fmt.Println("Número de CPUs:", runtime.NumCPU())
+	fmt.Println("Número de Goroutinas:", runtime.NumGoroutine())
+
+	contador := 0
+
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+	var mu sync.Mutex
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			mu.Lock()
+			v := contador
+			v++
+			runtime.Gosched()
+			contador = v
+			mu.Unlock()
+			wg.Done()
+		}()
+		fmt.Println("Número de Goroutinas:", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("Cuenta:", contador)
 }
