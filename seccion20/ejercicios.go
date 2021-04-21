7dc97c6a9f4de3499d2aff3bf77cd7b55c748637
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-21 11:14:10 -0500

    seccion 20 video 154 ejercicio#2 Gorutinas

--- seccion20/ejercicios.go
+++ seccion20/ejercicios.go
@@ -1,29 +1,29 @@
 package main
 
-import "fmt"
-
-type persona struct {
-	nombre string
-}
-
-func (p *persona) hablar() {
-	fmt.Println("Hola soy", p.nombre)
-}
-
-type humano interface {
-	hablar()
-}
-
-func diAlgo(h humano) {
-	h.hablar()
-}
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 
 func main() {
-	p1 := persona{
-		nombre: "Eduar",
+	var wg sync.WaitGroup
+
+	incremento := 0
+	gs := 100
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := incremento
+			runtime.Gosched()
+			v++
+			incremento = v
+			fmt.Println(incremento)
+			wg.Done()
+		}()
 	}
 
-	//diAlgo(p1)
-	//diAlgo(&p1)
-	p1.hablar()
+	wg.Wait()
+	fmt.Println("El valor final de incremento es:", incremento)
 }
