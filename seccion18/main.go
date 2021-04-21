5c4fd28b81987def6a82e8bbab391b3e58cd833a
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-21 08:56:03 -0500

    seccion 18 video 134 channels example with function anonima

--- seccion18/main.go
+++ seccion18/main.go
@@ -2,36 +2,16 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"sync"
 )
 
-var wg sync.WaitGroup
+func doSomething(x int) int {
+	return x * 5
+}
 
 func main() {
-	fmt.Println("OS\t\t", runtime.GOOS)
-	fmt.Println("ARCH\t\t", runtime.GOARCH)
-	fmt.Println("CPUs\t\t", runtime.NumCPU())
-	fmt.Println("Goroutines\t", runtime.NumGoroutine())
-
-	wg.Add(1)
-	go foo()
-	bar()
-
-	fmt.Println("CPUs\t\t", runtime.NumCPU())
-	fmt.Println("Goroutines\t", runtime.NumGoroutine())
-	wg.Wait()
-}
-
-func foo() {
-	for i := 0; i < 10; i++ {
-		fmt.Println("foo:", i)
-	}
-	wg.Done()
-}
-
-func bar() {
-	for i := 0; i < 10; i++ {
-		fmt.Println("bar:", i)
-	}
+	ch := make(chan int)
+	go func() {
+		ch <- doSomething(5)
+	}()
+	fmt.Println(<-ch)
 }
