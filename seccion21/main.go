92cf8d4d3a439a2915a711800bbef2f7df1255f2
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-21 14:44:34 -0500

    seccion 21 video 163 canales con select

--- seccion21/main.go
+++ seccion21/main.go
@@ -3,20 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	c := make(chan int)
+	par := make(chan int)
+	impar := make(chan int)
+	salir := make(chan int)
 
 	//enviar
-	go func() {
-		for i := 0; i < 5; i++ {
-			c <- i
-		}
-		close(c)
-	}()
+	go enviar(par, impar, salir)
+
 	//recibir
-	for v := range c {
-		fmt.Println(v)
+	recibir(par, impar, salir)
+	fmt.Println("Finalizado")
+
+}
+
+//Enviar
+func enviar(p, i, s chan<- int) {
+	for j := 0; j < 100; j++ {
+		if j%2 == 0 {
+			p <- j
+		} else {
+			i <- j
+		}
 	}
 
-	fmt.Println("Finalizado.")
+	s <- 0
+}
 
+//Recibir
+func recibir(p, i, s <-chan int) {
+	//hacemos un for infinito y lo rompemos segun las condiciones
+	for {
+		select {
+		case v := <-p:
+			fmt.Println("Desde el canal par:", v)
+		case v := <-i:
+			fmt.Println("Desde el canal impar:", v)
+		case v := <-s:
+			fmt.Println("Desde el canal salir:", v)
+			return
+		}
+	}
 }
