518872bf4d74ee350a2d388e4071f5c9d1cac6d2
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-25 14:06:02 -0500

    seccion 22 video 172 ejercicio #5 uso idioma coma ok

--- 22-seccion/ejercicios.go
+++ 22-seccion/ejercicios.go
@@ -5,35 +5,17 @@ import (
 )
 
 func main() {
-	salir := make(chan int)
-	c := gen(salir)
-
-	recibir(c, salir)
-
-	fmt.Println("A punto de finalizar.")
-}
-
-func recibir(c, salir <-chan int) {
-	for {
-		select {
-		case v := <-c:
-			fmt.Println(v)
-		case <-salir:
-			return
-		}
-	}
-}
-
-func gen(salir chan<- int) <-chan int {
 	c := make(chan int)
 
 	go func() {
-		for i := 0; i < 10; i++ {
-			c <- i
-		}
-		salir <- 1
-		close(c)
+		c <- 42
 	}()
 
-	return c
+	v, ok := <-c
+	fmt.Println(v, ok)
+
+	close(c)
+
+	v, ok = <-c
+	fmt.Println(v, ok)
 }
