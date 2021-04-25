10cb39d09388b84f25886fa5b01e6b28a4800a5a
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-25 15:05:06 -0500

    seccion 23 video 178 recover

--- 23-seccion/main.go
+++ 23-seccion/main.go
@@ -1,18 +1,29 @@
 package main
 
-import (
-	"os"
-)
+import "fmt"
 
 func main() {
-	_, err := os.Open("sin-archivo.txt")
-	if err != nil {
-		//		fmt.Println("un error ocurrió", err)
-		//		log.Println("un error ocurrió", err)
-		//		log.Fatalln(err)
-		//		log.Panicln(err)
-		panic(err)
-	}
+	f()
+	fmt.Println("Returned normally from f.")
 }
 
-// http://godoc.org/builtin#panic
+func f() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normally from g.")
+}
+
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v", i))
+	}
+	defer fmt.Println("Defer in g", i)
+	fmt.Println("Printing in g", i)
+	g(i + 1)
+}
