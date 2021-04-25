9bca0f53345c07f59229ad071ae5ff2753664412
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-25 12:23:07 -0500

    seccion 21 video 164 idioma coma ok ejemplo sencillo

--- seccion21/main.go
+++ seccion21/main.go
@@ -5,44 +5,17 @@ import (
 )
 
 func main() {
-	par := make(chan int)
-	impar := make(chan int)
-	salir := make(chan int)
+	c := make(chan int)
+	go func() {
+		c <- 42
+		close(c)
+	}()
 
-	go enviar(par, impar, salir)
+	v, ok := <-c
 
-	recibir(par, impar, salir)
+	fmt.Println(v, ok)
 
-	fmt.Println("Finalizar.")
-}
+	v, ok = <-c
 
-// send channel (canal para enviar)
-func enviar(par, impar, salir chan<- int) {
-	for i := 0; i < 10; i++ {
-		if i%2 == 0 {
-			par <- i
-		} else {
-			impar <- i
-		}
-	}
-	close(salir)
-}
-
-// receive channel (canal para recibir)
-func recibir(par, impar, salir <-chan int) {
-	for {
-		select {
-		case v := <-par:
-			fmt.Println("El valor recibido del canal par:", v)
-		case v := <-impar:
-			fmt.Println("El valor recibido del canal impar:", v)
-		case i, ok := <-salir:
-			if !ok {
-				fmt.Println("desde comma ok", i, ok)
-				return
-			} else {
-				fmt.Println("desde comma ok", ok)
-			}
-		}
-	}
+	fmt.Println(v, ok)
 }
