564357ef3ca197f9945b5e70b79202b64db0ceb8
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-25 15:12:06 -0500

    seccion 23 video 179 Errores con info ejemplo errors.New definiendola en variable

--- 23-seccion/main.go
+++ 23-seccion/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 )
 
+var ErrMat = errors.New("de matemática elemental: no hay raíz cuadrada real de un número negativo")
+
 func main() {
+	fmt.Printf("%T\n", ErrMat)
 	_, err := sqrt(-10)
 	if err != nil {
 		log.Fatalln(err)
@@ -14,7 +18,11 @@ func main() {
 
 func sqrt(f float64) (float64, error) {
 	if f < 0 {
-		return 0, errors.New("de matemática elemental: no hay raíz cuadrada real de un número negativo")
+		return 0, ErrMat
 	}
 	return 42, nil
 }
+
+// lee sobre el uso de errors.New en la biblioteca estándar:
+// http://golang.org/src/pkg/bufio/bufio.go
+// http://golang.org/src/pkg/io/io.go
