7c8abaa752027af640c6a25bd27e0e78b04d5b10
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-25 12:56:15 -0500

    seccion 21 video 165 Fan in pasar valores de varios canales a uno solo

--- seccion21/main.go
+++ seccion21/main.go
@@ -2,20 +2,57 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
-	c := make(chan int)
+	par := make(chan int)
+	impar := make(chan int)
+	fanin := make(chan int)
+
+	go enviar(par, impar)
+
+	go recibir(par, impar, fanin)
+
+	for v := range fanin {
+		fmt.Println(v)
+	}
+
+	fmt.Println("Finalizando.")
+}
+
+// send channel
+func enviar(par, impar chan<- int) {
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			par <- i
+		} else {
+			impar <- i
+		}
+	}
+	close(par)
+	close(impar)
+}
+
+// receive channel
+func recibir(par, impar <-chan int, fanin chan<- int) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
 	go func() {
-		c <- 42
-		close(c)
+		for v := range par {
+			fanin <- v
+		}
+		wg.Done()
 	}()
 
-	v, ok := <-c
+	go func() {
+		for v := range impar {
+			fanin <- v
+		}
+		wg.Done()
+	}()
 
-	fmt.Println(v, ok)
-
-	v, ok = <-c
-
-	fmt.Println(v, ok)
+	wg.Wait()
+	close(fanin)
 }
