ec610084cf496873e21534bcf36629518ef6b70c
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-04-25 14:10:41 -0500

    seccion 22 video 174 ejercicio #7 crear 10 gorutinas y enviar 10 numeros

--- 22-seccion/ejercicios.go
+++ 22-seccion/ejercicios.go
@@ -5,15 +5,16 @@ import "fmt"
 func main() {
 	c := make(chan int)
 
-	go func() {
-		for i := 0; i < 100; i++ {
-			c <- i
-		}
-		close(c)
-	}()
+	for j := 0; j < 10; j++ {
+		go func() {
+			for i := 0; i < 10; i++ {
+				c <- i
+			}
+		}()
+	}
 
-	for v := range c {
-		fmt.Println(v)
+	for k := 0; k < 100; k++ {
+		fmt.Println(k, <-c)
 	}
 
 	fmt.Println("A punto de finalizar.")
